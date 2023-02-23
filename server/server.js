fe03ffb8cde0b2d3fa5318b8eed3b76d4347c64e
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-02-22 18:46:12 -0800

    Setup Server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import http from 'http'
+
+
+const startApolloServer = async(typeDefs, resolvers) => {
+    const app = express()
+
+    const httpServer = http.createServer(app)
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
+    })
+
+    await server.start()
+
+    server.applyMiddleware({app})
+
+    await new Promise(resolve => httpServer.listen({port: 4000}, resolve))
+
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+}
